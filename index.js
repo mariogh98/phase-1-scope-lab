55b2d9c0883254535576cf3bf27312e2c33030d0
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-22 20:16:11 -0500

    commit tree 439080c1d9ccdaa6754e10129772f451898a3bb0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+
+
+
+function setBestCustomer() {
+     bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "Phillip"  /*<=====Value of the variable should not be able to be reassigned when decalred with (const)*/
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Gordon"
+}
